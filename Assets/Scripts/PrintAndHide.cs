d2dd88d0593ce9535fecf54b57e3cc2db367fc2b
LostCrypt <43767802+LostCrypt@users.noreply.github.com>
2020-08-17 19:21:56 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,18 +6,29 @@ public class PrintAndHide : MonoBehaviour
 {
     int i;
     public Renderer rend;
+    private int randInt;
 
     // Start is called before the first frame update
     void Start()
     {
         i = 0;
+        randInt = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
-
         Debug.Log(gameObject.name + " : " + i);
         i++;
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.tag == "Blue" && i == randInt)
+        {
+            rend.enabled = false;
+        }
     }
 }
